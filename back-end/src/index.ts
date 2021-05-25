5f9b36b5327f7cfaf082ce4dfd447e21fb97c107
SeungBo <47034129+SecretJuJu@users.noreply.github.com>
2021-05-25 13:26:53 +0900

    backend-skeleton

---
+++ back-end/src/index.ts
@@ -0,0 +1,19 @@
+import dotenv from "dotenv"
+dotenv.config()
+
+import express from "express"
+import swaggerUI from "swagger-ui-express"
+import { ErrorType, errorMessages } from "errors"
+import connectDB from "config/connectDB"
+connectDB()
+
+import app from "app"
+
+app.get("/custom-errors", (req: express.Request, res: express.Response) => {
+    res.json({
+        errorTypes: ErrorType,
+        errorMessages: errorMessages,
+    })
+})
+
+app.listen(process.env.PORT || 3000)
