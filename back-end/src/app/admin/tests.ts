f9aeaed79ce64ecc83b259ce0e9fae6a17893d97
SeungBo <47034129+SecretJuJu@users.noreply.github.com>
2021-05-26 08:49:22 +0900

    test: add test cases at register(success case & error cases)

--- back-end/src/app/admin/tests.ts
+++ back-end/src/app/admin/tests.ts
@@ -22,23 +22,63 @@ describe("Admin", async function () {
     })
 
     describe("Register", function () {
-        it("Normal Register",async ()=>{
-            const response = await request(app)
-            .post("/api/admin/register")
-            .send({
-                username: username,
-                email: email,
-                password: password,
-                secretCode: secretCode
+        describe("Success Cases", async function () {
+            it("Normal Register",async ()=>{
+                const response = await request(app)
+                .post("/api/admin/register")
+                .send({
+                    username: username,
+                    email: email,
+                    password: password,
+                    secretCode: secretCode
+                })
+    
+                if (response.body.result) {
+                    assert(true)
+                } else {
+                    console.log("===== Normal Register failed ======")
+                    console.log(response.body)
+                    assert(false)
+                }
             })
-
-            if (response.body.result) {
-                assert(true)
-            } else {
-                console.log("Normal Register failed")
-                console.log(response.body)
-                assert(false)
-            }
+        })
+        describe("Error Cases", async function () {
+            it("not valid email",async ()=>{
+                const response = await request(app)
+                .post("/api/admin/register")
+                .send({
+                    username: username,
+                    email: "dev.tmdq@@@@gmail.com",
+                    password: password,
+                    secretCode: secretCode
+                })
+                const { errorType } = JSON.parse(response.text)
+                assert.strictEqual(errorType, ErrorType.ValidationError)
+            })
+            it("not strong password",async ()=>{
+                const response = await request(app)
+                .post("/api/admin/register")
+                .send({
+                    username: username,
+                    email: email,
+                    password: "imnotstrongpassword",
+                    secretCode: secretCode
+                })
+                const { errorType } = JSON.parse(response.text)
+                assert.strictEqual(errorType, ErrorType.ValidationError)
+            })
+            it("already exist user",async ()=>{
+                const response = await request(app)
+                .post("/api/admin/register")
+                .send({
+                    username: username,
+                    email: email,
+                    password: password,
+                    secretCode: secretCode
+                })
+                const { errorType } = JSON.parse(response.text)
+                assert.strictEqual(errorType, ErrorType.UsernameExist)
+            })
         })
     })
 
