010c00140ed3fe064ebaeaa41b770183c1f7fafd
SeungBo <47034129+SecretJuJu@users.noreply.github.com>
2021-05-25 13:19:26 +0900

    change directory structure

---
+++ front-end/src/components/search/index.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+
+// search component must include text input form & axios things and remove this comment later !
+export class Search extends React.Component<{},{keyword: string}> {
+  constructor(props: any) {
+    super(props)
+    this.state = {keyword:''};
+
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange = (event: any) => {
+    this.setState({keyword:event.target.value});
+  }
+
+  handleSubmit = (event: any) => {
+    event.preventDefault();
+    console.log(this.state.keyword)
+  }
+  
+  render() {
+    return <div className="search-wrapper">
+      <form onSubmit={this.handleSubmit}>
+        <input type="text" value={this.state.keyword} onChange={this.handleChange}/>
+        <button type="submit">search</button>
+      </form>
+    </div>;
+  }
+}
