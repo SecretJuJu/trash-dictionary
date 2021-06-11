b28d69d94282df634380cab40d3cec1e9b14ed7d
SeungBo <47034129+SecretJuJu@users.noreply.github.com>
2021-06-11 20:30:42 +0900

    refactorying: 라우팅 구조 변경

--- front-end/src/shared/App.tsx
+++ front-end/src/shared/App.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Router, Switch } from 'react-router-dom';
 
 import Routes from '../pages'
 
@@ -7,15 +7,16 @@ import './App.css';
 class App extends Component {
   render () {
     return (
-      <div>
+      <Switch>
         <Route exact path="/" component={Routes.Home}/>
         <Route exact path="/login" component={Routes.Login}/>
         <Route exact path="/register" component={Routes.Register}/>
         <Route exact path="/logout" component={Routes.Logout}/>
-        <Route path="*">
+        <Route exact path="/newFeed" component={Routes.NewFeed}></Route>
+        <Route>
           <div> 404 Not Found </div>
         </Route>
-      </div>
+      </Switch>
     );
   }
 }
