431aeba887d63e11fc381a8cd1c2eacf2bfbdee6
SeungBo <47034129+SecretJuJu@users.noreply.github.com>
2021-06-13 19:20:08 +0900

    refactorying: bodyparser -> express bodyparser

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -1,5 +1,4 @@
 import express from "express"
-import bodyParser from "body-parser"
 import routes from "routes"
 import setPassport from "config/passport"
 import passport from "passport"
@@ -10,13 +9,8 @@ setPassport()
 const app = express()
 
 app.use(cors())
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.json({limit: "5mb"}))
 app.use(passport.initialize())
-app.use(express.json({
-    limit : "50mb"
-}))
-app.use(express.urlencoded({ limit:"50mb", extended: false }));
 app.use("", routes)
 
 export default app
